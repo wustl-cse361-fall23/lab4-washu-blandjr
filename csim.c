54cab0dd0c918ee868e07ce81bb70ab86936a665
washu-blandjr <b.carmen@shell.cec.wustl.edu>
2023-11-03 18:45:39 -0500

    Part A done

--- csim.c
+++ csim.c
@@ -1,12 +1,10 @@
 #include "cachelab.h"
-#include "getopt.h"
 #include "stdbool.h"
-#include "math.h"
 #include "stdio.h"
-#include "string.h"
+#include "getopt.h"
 #include "stdlib.h"
+#include "math.h"
 
-#define addrSize 64;  //8 byte address 
 
 //Global variables
 int hits;
@@ -26,16 +24,13 @@ struct Values {
     int b;      //block offset bits
 };
 
-struct Trace {
-    long address;
-};
-
 struct Line { 
-    long tag;
+    int tag;
+    int use;
     bool valid;
     bool dirty;
-    bool lru;
 };
+
 
 struct Set {
     struct Line *lines;
@@ -45,25 +40,30 @@ struct Cache {
     struct Set *sets; 
 };
 
+struct Policy
+{
+    int MRU;
+    int LRU;
+};
+
+#define addrSize 64  //8 byte address 
 
 //Helper Functions
 
 //makeCache allocates memory for cache (for use in initializing cache)
-struct Cache makeCache(int S, int E, int B){
+struct Cache makeCache(struct Values V){
     struct Cache newCache;
     struct Set newSet;
     struct Line newLine;
 
-    //alloc space for S sets
-    newCache.sets = (struct Set *) malloc(S * sizeof(struct Set));
+    newCache.sets = (struct Set *) malloc(V.S * sizeof(struct Set));
 
-    //alloc space for E lines in each of set
-    for (int i=0; i < S; i++){
-        newSet.lines = (struct Line *) malloc(E * sizeof(struct Line));
+    for (int i = 0; i < V.S; i++){
+        newSet.lines = (struct Line *) malloc (V.E * sizeof(struct Line));
         newCache.sets[i] = newSet;
 
-        //set initial characteristics of each line
-        for (int j; i < E; j++){
+        for (int j = 0; j < V.E; j++){
+            newLine.use = 0;
             newLine.valid = false;
             newLine.dirty = false;
             newLine.tag = 0;
@@ -72,84 +72,179 @@ struct Cache makeCache(int S, int E, int B){
     }
     return newCache;
 }
-
 //freeCache frees space allocated for the cache
-void freeCache(struct Cache thisCache, int S, int E, int B){
-    for (int i = S; i >= 0; i--){     
-        if(thisCache.sets[i].lines != NULL){
+void freeCache(struct Cache thisCache, struct Values V){
+    for (int i = V.S; i >= 0; i--){     
+        if (thisCache.sets[i].lines != NULL){
             free(thisCache.sets[i].lines);
         }
     }
 
     //free space for sets once alloc'd space for lines are freed
-    if(thisCache.sets != NULL){
+    if (thisCache.sets != NULL){
         free(thisCache.sets);
     }    
 }
 
+//isHit checks to see if there has been a hit
+bool isHit(struct Line thisCache, int address){
+    return thisCache.tag == address && thisCache.valid;
+}
+
 //setNotFull checks if a valid bit in set has not been set to true
-bool setNotFull(struct Set thisSet){
-    struct Values V;
+bool setNotFull(struct Set thisSet, struct Values V){
     for (int i=0; i<V.E; i++){
-        if(!set.lines[i].valid){
+        if (!thisSet.lines[i].valid){
             return true;
         }
     }
     return false; //all valid bits set to true, so it's full
 }
 
-//emptyLineIndex checks for empty lines in a specifc set in the case of a cold miss
-int emptyLineIndex(struct Set thisSet){
-    struct Values V;
-    struct Line thisLine;
-
+//emptyLineIndex checks for empty lines in a set in the case of cold miss
+int emptyLineIndex(struct Set thisSet, struct Values V){
     for (int i = 0; i < V.E; i++){
-        if(!thisSet.lines[i].valid){
+        if (!thisSet.lines[i].valid){
             return i;
         }
     }
     return 0;
 }
 
-int LRUindex(struct Set thisSet){
+//recentlyUsed allows for finding most recently used and least recently used
+struct Policy recentlyUsed(struct Set thisSet, struct Values V){
+    struct Policy P;
+    int lruItem = thisSet.lines[0].use;
+    int mruItem = thisSet.lines[0].use;
+    P.LRU = 0;
+    P.MRU = 0;
 
-}
-
-int LRUindex(struct Set thisSet){
-    
-}
-
-//Cache Operations
-//TODO: make function for load and store
-void load(struct Cache thisCache, long address){
-    struct Values V;
-    int t = addrSize - V.s - V.b;
-    long addressTag = address >> (V.s + V.b);
-    long setVal = (address << (t)) >> (t + V.b); 
-    struct Set thisSet = thisCache.sets[setVal]; 
-
-    for (int i = 0; i < V.E; i++) {
-        struct Line thisLine = thisSet.lines[i]; 
-        if ((thisLine.tag == address) && (thisLine.valid)) { // Hit
-            hits++;
+    for (int i=0; i < V.E; i++){
+        if (thisSet.lines[i].use < lruItem){
+            P.LRU = i;
+            lruItem = thisSet.lines[i].use;
         }
     }
 
+    for (int i=0; i < V.E; i++){
+        if (thisSet.lines[i].use > mruItem){
+            P.MRU = i;
+            mruItem = thisSet.lines[i].use;
+        }
+    }
+    return P;
 }
 
-void store(struct Cache thisCache, long address){
+//Cache Operations
 
+void load(struct Cache thisCache, unsigned int address, struct Values V){
+    int t = addrSize - V.s - V.b;
+    int addressTag = address >> (V.s + V.b);
+    long setVal = address << t >> (t + V.b); 
+    struct Set thisSet = thisCache.sets[setVal]; 
+    int thisMRU = recentlyUsed(thisSet, V).MRU;
+    int thisLRU = recentlyUsed(thisSet, V).LRU;
+    
+    bool hit = false;
+    
+    for (int i = 0; i < V.E; i++) {
+        struct Line thisLine = thisSet.lines[i];
+        
+        if (isHit(thisLine, addressTag)) {
+            hits++;
+            hit = true;
+        
+            if (thisMRU == i){ //for if we JUST accessed this block
+                double_accesses++;
+            }
+        
+            thisCache.sets[setVal].lines[i].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+            break;
+        }
+    }
 
+    if (!hit && setNotFull(thisSet, V)) { //cold miss
+        misses++;
+        int emptyLine = emptyLineIndex(thisSet, V);
+        thisSet.lines[emptyLine].tag = addressTag;
+        thisSet.lines[emptyLine].dirty = false;
+        thisSet.lines[emptyLine].valid = true;
+        thisCache.sets[setVal].lines[emptyLine].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+
+    }
+
+    else if (!hit) {//conflict miss
+        misses++;
+        evictions++;
+        
+        if(thisSet.lines[thisLRU].dirty) {
+            dirty_evicted += pow(2.0, V.b);
+        }
+        
+        thisSet.lines[thisLRU].tag = addressTag;
+        thisSet.lines[thisLRU].dirty = false;
+        thisCache.sets[setVal].lines[thisLRU].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+    }
 }
-///
+
+void store(struct Cache thisCache, unsigned int address, struct Values V){
+    int t = addrSize - V.s - V.b;
+    int addressTag = address >> (V.s + V.b);
+    long setVal = address << t >> (t + V.b); 
+    struct Set thisSet = thisCache.sets[setVal]; 
+    int thisMRU = recentlyUsed(thisSet, V).MRU;
+    int thisLRU = recentlyUsed(thisSet, V).LRU;
+    
+    bool hit = false;
+
+    for (int i = 0; i < V.E; i++) {
+        struct Line thisLine = thisSet.lines[i];
+        
+        if (isHit(thisLine, addressTag)) {
+            hit = true;
+            hits++;
+
+            if (thisMRU == i){ //for if we JUST accessed this block
+                double_accesses++;
+            }
+
+            thisCache.sets[setVal].lines[i].dirty = true;
+            thisCache.sets[setVal].lines[i].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+            break;
+        }
+    }
+
+        
+        if (!hit && setNotFull(thisSet, V)) { //cold miss
+            misses++;
+
+            int emptyLine = emptyLineIndex(thisSet, V);
+
+            thisSet.lines[emptyLine].tag = addressTag;
+            thisSet.lines[emptyLine].dirty = true;
+            thisSet.lines[emptyLine].valid = true;
+            thisCache.sets[setVal].lines[emptyLine].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+
+        }
+
+       
+        else if (!hit) { //conflict miss
+            misses++;
+            evictions++;
+
+            if(thisSet.lines[thisLRU].dirty) {
+                dirty_evicted += pow(2.0, V.b);
+            }
+
+            thisSet.lines[thisLRU].tag = addressTag;
+            thisSet.lines[thisLRU].dirty = true;
+            thisCache.sets[setVal].lines[thisLRU].use = thisCache.sets[setVal].lines[thisMRU].use + 1;
+        }
+}
+
 int main(int argc, char * argv[]){  
+    struct Cache simCache;
     struct Values V;
-    
-    V.S = pow(2.0, V.s); //calc S = 2^s
-    V.B = pow(2.0, V.b); //calc B = 2^b
-    
-    // iniitializes the cache
-    struct Cache simCache = makeCache(V.S, V.E, V.B);
     
     //gets params s,E,b,t,v, and h from cmd line argument
     char opt; // hold current arg from cmd
@@ -158,6 +253,9 @@ int main(int argc, char * argv[]){
     //TODO: if time allows, work on v and h args
     while ((opt = getopt(argc, argv, "s:E:b:t:vh")) != -1) {
         switch (opt) {
+            case 'h':
+                exit(0);
+                break;
             case 's':
                 V.s = strtoul(optarg, (char **) trace, 10); //gets s value
                 break;
@@ -173,37 +271,54 @@ int main(int argc, char * argv[]){
         }
     }
 
-    // reading from trace file
+    V.B = pow(2.0, V.b); //calc B = 2^b
+    V.S = pow(2.0, V.s); //calc S = 2^s
+    
+    // iniitializes the cache
+    simCache = makeCache(V);
+    
+    
+     //reading from trace file
     FILE * f = fopen(trace, "r");
     char op;
     int size;
-    unsigned long addr;
+    unsigned int addr;
+    
+    if (f != NULL) {
+        while (fscanf(f, "%c %x, %d", &op, &addr, &size) != EOF)
+        {
+            switch (op) {
+            case 'I': //ignore I
+                break;
+            case 'L':
+                load(simCache, addr, V);
+                break;
+            
+            case 'S':
+                store(simCache, addr, V);
+                break;
 
-    while (fscanf(f, "%c %lx, %d", &op, &addr, &size) != EOF)
-    {
-        switch (op) {
-        case 'I': //ignore I
-            break;
-        case 'L':
-            //load(simCache, Values, addr);
-            break;
-        
-        case 'S':
-            //store(simCache, Values, addr);
-            break;
-
-        case 'M':
-            //load(simCache, Values, addr);
-            //store(simCache, Values, addr);
-            break;
+            case 'M':
+                load(simCache, addr, V);
+                store(simCache, addr, V);
+                break;
+            }
+        }
+    }
+    
+    //for counting the active dirty bytes
+    for (int i = 0; i < pow(2.0, V.s); i++) {
+        for (int j = 0; j < V.E; j++) {
+            if (simCache.sets[i].lines[j].dirty) {
+                dirty_active += pow(2.0, V.b);
+            }
         }
     }
 
     printSummary(hits, misses, evictions, dirty_evicted, dirty_active, double_accesses);
     
-    // to prevent memory leak, free up the space held up cache
-    freeCache(simCache, V.S, V.E, V.B);
+    //to prevent memory leak, free up the space held up cache and close file
+    freeCache(simCache,V);
     fclose(f);
-
     return 0;
 }
